1dce01d2124f236503d0358dc5b90244f717841b
Charles Doebler <cdoebler1@gmail.com>
2022-10-21 15:48:48 -0400

    added keras/tensorflow version

---
+++ image_preprocessing_keras.py
@@ -0,0 +1,20 @@
+"""
+Created on Thur Oct 20
+
+@author: CMPSC445 WCFa22 Group 2
+"""
+import os
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import img_to_array
+import numpy as np
+
+file = open("angry.csv", "w")
+basedir = ('dataset/train/angry/')
+with os.scandir(basedir) as faces:
+    for face in faces:
+        if face.is_file():
+            img = load_img(basedir + face.name, color_mode = "grayscale")
+            np_img = img_to_array(img)[:,:,0]
+print(np.shape(np_img))
+np.savetxt(file,np_img)
+file.close()
