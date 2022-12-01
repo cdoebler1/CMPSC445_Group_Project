152d5063c46b9d6f8c15e9fbd5ff427cf3a1a6ee
Charles Doebler <cdoebler1@gmail.com>
2022-12-01 14:15:07 -0500

    Clean demo using TF Keras APIs
    This is a very clean demo with tight code.

--- CMPSC445_WCFa22_Group2.py
+++ CMPSC445_WCFa22_Group2.py
@@ -1,6 +1,45 @@
+#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Sun Oct 16 16:47:56 2022
 
 @author: CMPSC445 WCFa22 Group 2
 """
+
+import preprocessor as pp
+import tensorflow as tf
+
+# Preprocess the data set
+train_data = pp.preprocess('dataset/train')
+test_data = pp.preprocess('dataset/test')
+
+# Display a few sample images from the training and test data sets
+num_images = 9
+pp.image_sample_display(train_data, num_images)
+pp.image_sample_display(test_data, num_images)
+
+# Train the model
+# num_classes = 7
+num_classes = len(train_data.class_names)
+
+model = tf.keras.Sequential([
+  tf.keras.layers.Rescaling(1./255),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Conv2D(32, 3, activation='relu'),
+  tf.keras.layers.MaxPooling2D(),
+  tf.keras.layers.Flatten(),
+  tf.keras.layers.Dense(128, activation='relu'),
+  tf.keras.layers.Dense(num_classes)])
+
+model.compile(
+  optimizer='adam',
+  loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+  metrics=['accuracy'])
+
+model.fit(
+  train_data,
+  validation_data=test_data,
+  epochs=3)
---
+++ preprocessor.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 22 22:20:46 2022
+
+@author: CMPSC445 WCFa22 Group 2
+"""
+
+from tensorflow.keras.utils import image_dataset_from_directory
+import matplotlib.pyplot as plt
+
+def preprocess(data_dir):
+    data_set = image_dataset_from_directory(data_dir, labels = 'inferred', label_mode = 'int',
+                                          class_names=None, color_mode = "grayscale",
+                                          batch_size=32, image_size=(48,48), shuffle=True,
+                                          seed=None, validation_split=None, subset=None,
+                                          interpolation='bilinear', follow_links=False,
+                                          crop_to_aspect_ratio=False)
+    return(data_set)
+
+def image_sample_display(data_set, num_images):
+    class_names = data_set.class_names
+    plt.figure(figsize=(10, 10))
+    for images, labels in data_set.take(1):
+        for i in range(num_images):
+            ax = plt.subplot(3, 3, i + 1)
+            plt.imshow(images[i].numpy().astype("uint8"))
+            plt.title(class_names[labels[i]])
+            plt.axis("off")
+
+            
